a6b19ab891e732112ab55ce8e2ec5f4cd047da87
rsBio <dunedimensia@gmail.com>
2016-10-18 22:27:19 +0300

    is_matrix.pl - tikrina ar lentele staciakampe, isveda ismatavimus

---
+++ Matricos/Matricu-palyg/is_matrix.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $debug = 0;
+
+my @ARGV_2;
+my @opt;
+
+for (@ARGV){
+	/^-\S/ ? (push @opt, $_) : (push @ARGV_2, $_);
+}
+
+my $split = '\t';
+
+for (@opt){
+	/-tsv/ and do {
+		$split = '\t';
+	};
+	/-csv/ and do {
+		$split = ',';
+	};
+	/-cssv/ and do {
+		$split = ', ';
+	};
+	/-ssv/ and do {
+		$split = ' ';
+	};
+	/-d/ and $debug = 1;
+}
+
+@ARGV = @ARGV_2;
+
+for (@ARGV){
+	my $in;
+	/^-$/ or open $in, '<', $_ or die "$0: [$_] ... : $!\n";
+	my @lengths = map { 0 + split /$split/ } (defined $in ? <$in> : <STDIN>);
+    
+    my $rows = @lengths;
+    my $ok = @lengths == grep { $lengths[0] == $_ } @lengths;
+    
+    print $ok ? "Matrix: ${lengths[0]} (columns) x ${rows} (rows)\n"
+        : "Not a matrix\n";
+}
