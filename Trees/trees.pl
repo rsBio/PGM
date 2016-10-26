6d04db372daf28de84898cd4ff05037fa3e7fdbe
rsBio <dunedimensia@gmail.com>
2016-10-27 01:17:32 +0300

    pagerinau skaiciavimu tiksluma ir jo parametrus

--- Trees/trees.pl
+++ Trees/trees.pl
@@ -2,13 +2,16 @@
 
 use warnings;
 use strict;
+srand;
 
 sub findxy {
 	my( $Ax, $Ay, $Bx, $By, $ab, $ac, $bc ) = @_;
 	my( $x, $y ) = (0, 0);
-	for my $i (1 .. 5){
+	for my $i (1 .. 80){
 	my $step = 1;
-	while( $step > 0.001 ){
+	while( $step > 0.000000001 ){
+	#	use bignum;
+	#	my $sort = int rand(2);
 		my ($ref_closer) = 
 		# Schwartzian transform:
 		map { [ $_->[0], $_->[1] ] }
@@ -26,11 +29,16 @@ sub findxy {
 			[ $x + $step, $y - $step ],
 			[ $x - $step, $y + $step ],
 			[ $x - $step, $y - $step ],
+			[ $x + $step, $y + 0 ],
+			[ $x + $step, $y - 0 ],
+			[ $x - 0, $y + $step ],
+			[ $x - 0, $y - $step ],
 		)[ 0 ];
-			
+	#	no bignum;	
 		($x, $y) = @{$ref_closer};
 	#	print "[$x|$y]\n";
-		$step *= 0.7;
+	#	$step *= 0.7 + (0.2 * rand(1));
+		$step *= 0.5;
 	}
 	}
 	"[$x|$y]\n"
@@ -39,3 +47,7 @@ sub findxy {
 #print findxy( $Ax, $Ay, $Bx, $By, $ab, $ac, $bc );
 #findxy( 0, 0, 2, 0, 2, 5, 4 ) for 1 .. 1000;
 print findxy( 0, 0, 2, 0, 2, 5, 4 );
+while( <> ){
+	print findxy( split );
+}
+
