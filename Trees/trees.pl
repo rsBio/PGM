d28c7a52def3b199c1103ba8a9f37808eb25a47e
rsBio <dunedimensia@gmail.com>
2016-10-27 00:03:38 +0300

    iskeliau sorto funkcija i Schwartzian transformacija, bet programa deja nepagreitejo

--- Trees/trees.pl
+++ Trees/trees.pl
@@ -4,38 +4,38 @@ use warnings;
 use strict;
 
 sub findxy {
-    my( $Ax, $Ay, $Bx, $By, $ab, $ac, $bc ) = @_;
-    my( $x, $y ) = (0, 0);
-    for my $i (1 .. 5){
-    my $step = 1;
-    while( $step > 0.001 ){
-        my $ref_closer = 
-        (
-        sort {
-            my( $x1, $y1 ) = @{ $a };
-                my $A =
-                abs( $ac - sqrt( ($Ax - $x1)**2 + ($Ay - $y1)**2 ) ) + 
-                abs( $bc - sqrt( ($Bx - $x1)**2 + ($By - $y1)**2 ) );
-            my( $x2, $y2 ) = @{ $b };
-                my $B = 
-                abs( $ac - sqrt( ($Ax - $x2)**2 + ($Ay - $y2)**2 ) ) + 
-                abs( $bc - sqrt( ($Bx - $x2)**2 + ($By - $y2)**2 ) );
-            
-            $A <=> $B
-            }
-        [ $x, $y ],
-        [ $x + $step, $y + $step ],
-        [ $x + $step, $y - $step ],
-        [ $x - $step, $y + $step ],
-        [ $x - $step, $y - $step ],
-        )[ 0 ];
-        
-        ($x, $y) = @{$ref_closer};
-        print "[$x|$y]\n";
-        $step *= 0.7;
-    }
-    }
+	my( $Ax, $Ay, $Bx, $By, $ab, $ac, $bc ) = @_;
+	my( $x, $y ) = (0, 0);
+	for my $i (1 .. 5){
+	my $step = 1;
+	while( $step > 0.001 ){
+		my ($ref_closer) = 
+		# Schwartzian transform:
+		map { [ $_->[0], $_->[1] ] }
+		(
+		sort { $a->[2] <=> $b->[2] }
+		map {
+			my( $x1, $y1 ) = @{ $_ };
+			[ $x1, $y1,
+			abs( $ac - sqrt( ($Ax - $x1)**2 + ($Ay - $y1)**2 ) ) +
+			abs( $bc - sqrt( ($Bx - $x1)**2 + ($By - $y1)**2 ) )
+			]
+		}
+			[ $x, $y ],
+			[ $x + $step, $y + $step ],
+			[ $x + $step, $y - $step ],
+			[ $x - $step, $y + $step ],
+			[ $x - $step, $y - $step ],
+		)[ 0 ];
+			
+		($x, $y) = @{$ref_closer};
+	#	print "[$x|$y]\n";
+		$step *= 0.7;
+	}
+	}
+	"[$x|$y]\n"
 }
 
 #print findxy( $Ax, $Ay, $Bx, $By, $ab, $ac, $bc );
+#findxy( 0, 0, 2, 0, 2, 5, 4 ) for 1 .. 1000;
 print findxy( 0, 0, 2, 0, 2, 5, 4 );
