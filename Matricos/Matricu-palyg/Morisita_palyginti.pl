8f39a47ee8923edcfd5ada9d0a653bcff6ee4a68
rsBio <dunedimensia@gmail.com>
2016-10-30 19:29:57 +0200

    Prapleciau programa opcijomis; pakeiciau kintamuju pavadinimus; Patiklsinau formule (Simpsono lambdos).

--- Matricos/Matricu-palyg/Morisita_palyginti.pl
+++ Matricos/Matricu-palyg/Morisita_palyginti.pl
@@ -3,55 +3,102 @@
 use strict;
 use warnings;
 
-$\ = $/;
+my $debug = 0;
 
-my ($ilgis, $dim) = split ' ', <>;
+my @ARGV_2;
+my @opt;
 
-my @i_dim = map { [split] } grep m/\S/, <>;
-
-#%  print "@{$_}" for @i_dim;
-
-my @Nb;
-
-for my $i (1 .. $ilgis){
-    my $sum;
-    for my $j (1 .. $dim){
-        $sum += $i_dim[ $j-1 ][ $i-1 ];
-    #%    print "  $i $j: $sum";
-    }
-    push @Nb, $sum;
+for (@ARGV){
+	/^-\S/ ? (push @opt, $_) : (push @ARGV_2, $_);
 }
 
-#%  print "@Nb";
+my $split = " ";
+my $join = ",";
 
-my @lambda;
-
-for my $i (1 .. $ilgis){
-    my $sum;
-    for my $j (1 .. $dim){
-        $sum += ( $i_dim[ $j-1 ][ $i-1 ] / $Nb[ $i-1 ] ) ** 2;
-    #%    print "  $i $j: $sum";
-    }
-#%    printf "    lambda b[%d]: %s\n", $i, $sum;
-    push @lambda, $sum;
+for (@opt){
+	/-tsv/ and do {
+		$split = '\t';
+	};
+	/-csv/ and do {
+		$split = ',';
+	};
+	/-cssv/ and do {
+		$split = ', ';
+	};
+	/-ssv/ and do {
+		$split = ' ';
+	};
+	/-totsv/ and do {
+		$join = '\t';
+	};
+	/-tocsv/ and do {
+		$join = ',';
+	};
+	/-tocssv/ and do {
+		$join = ', ';
+	};
+	/-tossv/ and do {
+		$join = ' ';
+	};
+	/-d/ and $debug = 1;
 }
 
-#%  print "@lambda";
+@ARGV = @ARGV_2;
 
-my @matrix;
+for (@ARGV){
+	my $in;
+	/^-$/ or open $in, '<', $_ or die "$0: [$_] ... : $!\n";
+	my @data = map { chomp; [ split /$split/ ] } grep m/./, (defined $in ? <$in> : <STDIN>);
 
-for my $i (1 .. $ilgis){
-    my @line;
-    for my $j (1 .. $ilgis){
-        my $sum;
-        for my $r (@i_dim){
-            $sum += $r->[ $i-1 ] * $r->[ $j-1 ];
-        #%    print "$i $j :$C";
-        }
-        push @line, 2 * $sum / 
-            ( ($lambda[ $i-1 ] + $lambda[ $j-1 ]) * $Nb[ $i-1 ] * $Nb[ $j-1 ] );
-    }
-    push @matrix, [ @line ];
+	my( $gylis, $dim ) = @{ shift @data };
+	$gylis != @{ $data[0] } and 
+		die "$0: gylis ($gylis) != num of columns. Maybe incorrect split?\n";
+	
+	my @dim_gylis = @data;
+
+	$debug and print "@{$_}\n" for @dim_gylis;
+
+	my @Xi;
+
+	for my $i (1 .. $gylis){
+		my $sum;
+		for my $j (1 .. $dim){
+		    $sum += $dim_gylis[ $j-1 ][ $i-1 ];
+		#%    print "  $i $j: $sum";
+		}
+		push @Xi, $sum;
+	}
+
+	my @lambda_i;
+
+	for my $i (1 .. $gylis){
+		my $sum;
+		for my $j (1 .. $dim){
+		    $sum += ( $dim_gylis[ $j-1 ][ $i-1 ] ** 2 - $dim_gylis[ $j-1 ][ $i-1 ] ) /
+					( $Xi[ $i-1 ]                ** 2 - $Xi[ $i-1 ] );
+		}
+	#%	printf "    lambda_i [%d]: %s\n", $i, $sum;
+		push @lambda_i, $sum;
+	}
+
+	#%  print "@lambda_i";
+
+	my @matrix;
+
+	for my $i (1 .. $gylis){
+		my @line;
+		for my $j (1 .. $gylis){
+		    my $sum;
+		    for my $r (@dim_gylis){
+		        $sum += $r->[ $i-1 ] * $r->[ $j-1 ];
+		    }
+		    push @line, 2 * $sum / 
+		        ( ($lambda_i[ $i-1 ] + $lambda_i[ $j-1 ]) * $Xi[ $i-1 ] * $Xi[ $j-1 ] );
+		}
+		push @matrix, [ @line ];
+	}
+
+	print map "$_\n", join "$join", @{$_} for @matrix;
+
 }
 
-print join ',', @{$_} for @matrix;
