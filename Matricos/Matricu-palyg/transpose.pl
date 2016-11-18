bd9c96954ee0a052dec20ae4d4873689474b33d6
rsBio <dunedimensia@gmail.com>
2016-11-18 11:12:19 +0200

    transpose.pl

---
+++ Matricos/Matricu-palyg/transpose.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $debug = 0;
+
+my @ARGV_2;
+my @opt;
+
+for (@ARGV){
+	/^-\S/ ? (push @opt, $_) : (push @ARGV_2, $_);
+}
+
+my $split = " ";
+my $join = " ";
+
+for (@opt){
+	/-tsv/ and do {
+		$split = "\t";
+	};
+	/-csv/ and do {
+		$split = ',';
+	};
+	/-cssv/ and do {
+		$split = ', ';
+	};
+	/-ssv/ and do {
+		$split = ' ';
+	};
+	/-nosep/ and do {
+		$split = '';
+	};
+	/-totsv/ and do {
+		$join = "\t";
+	};
+	/-tocsv/ and do {
+		$join = ',';
+	};
+	/-tocssv/ and do {
+		$join = ', ';
+	};
+	/-tossv/ and do {
+		$join = ' ';
+	};
+	/-tonosep/ and do {
+		$join = '';
+	};
+	/-d/ and $debug = 1;
+}
+
+@ARGV = @ARGV_2;
+
+for (@ARGV){
+	my $in;
+	/^-$/ or open $in, '<', $_ or die "$0: [$_] ... : $!\n";
+	
+	my @data;
+	
+	for (defined $in ? <$in> : <STDIN>){
+		my $i = 0;
+		chomp;
+		for ( split /$split/ ){
+			push @{$data[ $i++ ]}, $_;
+		}		
+	}
+
+	do { local $\ = $/; print join $join, @{$_} } for @data;
+}
+
