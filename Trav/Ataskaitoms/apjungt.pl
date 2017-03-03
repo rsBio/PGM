19a2c4e888fe109faca4e8669e322e541c52df76
rsBio <dunedimensia@gmail.com>
2017-03-03 17:39:31 +0200

    apjungt.pl - iškerpa ir apjungia ataskaitas į vieną, kurią reik dar paduot į travian-reports.net

---
+++ Trav/Ataskaitoms/apjungt.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $debug = 0;
+
+my @ARGV_2;
+my @opt;
+
+for (@ARGV){
+	/^-\S/ ? (push @opt, $_) : (push @ARGV_2, $_);
+}
+
+my $split = "\t";
+my $join = "\n";
+
+for (@opt){
+	/-tsv/ and do {
+		$split = "\t";
+	};
+	/-csv/ and do {
+		$split = ',';
+	};
+	/-cssv/ and do {
+		$split = ', ';
+	};
+	/-ssv/ and do {
+		$split = ' ';
+	};
+	/-d$/ and $debug = 1;
+}
+
+@ARGV = @ARGV_2;
+
+for (@ARGV){
+	my $in;
+	/^-$/ or open $in, '<', $_ or die "$0: [$_] ... : $!\n";
+	my $data = do { local $/; defined $in ? <$in> : <STDIN> };
+	
+	@_ = $data =~ /
+		Aukos(?:\t\d+)+\n
+		Informacija
+		.*?
+		(?=Grobis)
+	/sgx;
+	
+	s/
+		(Nurodytas .*? \n)
+		
+	/---$1\t/sgx
+		for @_;
+
+	s/
+		Aukos(?:\t\d+)+\n
+		Informacija
+	
+	//sgx
+		for @_[1 .. $#_];
+	
+#%	print @_;
+	
+	$data =~ /Tema: .*? Lojalumas/sx;
+	$data = $&;
+	$data =~ s/Aukos .*? (?=Grobis)/@_/sgx;
+	
+	print $data, "\n";
+}
