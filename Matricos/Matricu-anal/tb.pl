8e4a05a684f7e7687b8990c8c85424ccfd20cc4b
rsBio <dunedimensia@gmail.com>
2017-05-12 11:45:02 +0300

    improved to handle not only PBM but plain matrices. +minors

--- Matricos/Matricu-anal/tb.pl
+++ Matricos/Matricu-anal/tb.pl
@@ -5,34 +5,61 @@ use strict;
 
 my $debug = 0;
 
-my @ARGV_2;
+my @FILES;
 my @opt;
 
 for (@ARGV){
-	/^-\S/ ? (push @opt, $_) : (push @ARGV_2, $_);
+	/^-\S/ ? (push @opt, $_) : (push @FILES, $_);
 }
 
+my $split = " ";
+my $pbm = 0;
+my $to_pbm = 0;
 
 for (@opt){
-	;
+	/-pbm/ and do {
+		$pbm = 1;
+	};
+	/-topbm/ and do {
+		$to_pbm = 1;
+	};
+	/-tsv/ and do {
+		$split = "\t";
+	};
+	/-csv/ and do {
+		$split = ',';
+	};
+	/-cssv/ and do {
+		$split = ', ';
+	};
+	/-ssv/ and do {
+		$split = ' ';
+	};
+	/-nosep/ and do {
+		$split = '';
+	};
 }
 
-@ARGV = @ARGV_2;
-
-for (@ARGV){
+for (@FILES){
 	my $in;
 	/^-$/ or open $in, '<', $_ or die "$0: [$_] ... : $!\n";
 	my @data = grep m/./, (defined $in ? <$in> : <STDIN>);
 	chomp @data;
 	
-	my $pbm = 1;
 	my( $rows, $cols );
+		
 	if( $pbm ){
 		shift @data;
 		( $rows, $cols ) = reverse split ' ', shift @data;
 	}
 	
-	print "P1\n";
-	print "$cols $rows\n";
+	if( $to_pbm and ! $pbm ){
+		( $rows, $cols ) = ( ~~ @data, ~~ split $split, $data[0] );
+		}
+	
+	if( $to_pbm ){
+		print "P1\n";
+		print "$cols $rows\n";
+	}
 	print "$_\n" for reverse @data;
 }
