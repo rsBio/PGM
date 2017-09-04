2684445184805f9dcb370b82b1a1f02bf9add1c3
rsBio <dunedimensia@gmail.com>
2017-09-04 13:19:47 +0300

    +perl default sort

---
+++ Rikiavimai/perl_default_sort.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $debug = 0;
+
+my @FILES;
+my @opt;
+
+for (@ARGV){
+	/^-\S/ ? (push @opt, $_) : (push @FILES, $_);
+}
+
+my $split = " ";
+
+for (@opt){
+	/-tsv/ and do {
+		$split = "\t";
+	};
+	/-csv/ and do {
+		$split = ',';
+	};
+	/-cssv/ and do {
+		$split = ', ';
+	};
+	/-ssv/ and do {
+		$split = ' ';
+	};
+	/-nosep/ and do {
+		$split = '';
+	};
+	/-d$/ and do {
+		$debug = '';
+	};
+}
+
+for (@FILES){
+	my $in;
+	/^-$/ or open $in, '<', $_ or die "$0: [$_] ... : $!\n";
+	my @data = map { chomp; [ split $split ] } 
+		grep m/./, (defined $in ? <$in> : <STDIN>);
+	
+	print map "$_\n", join ' ', sort { $a <=> $b } ( @{$_} ) for @data;
+}
