fb4572dd2f624ea918a5aa52b22020c0861e83a8
rsBio <dunedimensia@gmail.com>
2018-09-10 13:42:53 +0300

    traverse_median.pl +ppm, +minor

--- Matricos/Matricu-anal/traverse_median.pl
+++ Matricos/Matricu-anal/traverse_median.pl
@@ -3,20 +3,21 @@
 use warnings;
 use strict;
 
-my $debug = 0;
+my $debug = 1;
 
-my @ARGV_2;
+my @FILES;
 my @opt;
 
 for (@ARGV){
-	/^-\S/ ? (push @opt, $_) : (push @ARGV_2, $_);
+	/^-\S/ ? (push @opt, $_) : (push @FILES, $_);
 }
 
 my $split = " ";
 my $join = " ";
 my $pbm = 0;
-my $to_pbm = 0;
+my $to_pbm = 0;  # not relevant
 my $to_pgm = 0;
+my $to_ppm = 0;
 my $row_step = 10;
 my $col_step = 10;
 my $step_proc_on = 1;
@@ -25,6 +26,15 @@ my $step_proc = 20;
 for (@opt){
 	/-pbm/ and do {
 		$pbm = 1;
+	};
+	/-topbm/ and do {
+		$to_pbm = 1;
+	};
+	/-topgm/ and do {
+		$to_pgm = 1;
+	};
+	/-toppm/ and do {
+		$to_ppm = 1;
 	};
 	/-stepproc(\d+)/ and do {
 		$step_proc_on = 1;
@@ -42,12 +52,6 @@ for (@opt){
 		$step_proc_on = 0;
 		$col_step = $1;
 		$row_step = $1;
-	};
-	/-topbm/ and do {
-		$to_pbm = 1;
-	};
-	/-topgm/ and do {
-		$to_pgm = 1;
 	};
 	/-tsv/ and do {
 		$split = "\t";
@@ -82,9 +86,7 @@ for (@opt){
 	/-d$/ and $debug = 1;
 }
 
-@ARGV = @ARGV_2;
-
-for (@ARGV){
+for (@FILES){
 	my $in;
 	/^-$/ or open $in, '<', $_ or die "$0: [$_] ... : $!\n";
 	my @data = grep m/./, (defined $in ? <$in> : <STDIN>);
@@ -96,7 +98,7 @@ for (@ARGV){
 		( $rows, $cols ) = reverse split ' ', shift @data;
 	}
 	else{
-		( $rows, $cols ) = (0 + @data, 0 + split /$split/, $data[0] );
+		( $rows, $cols ) = ( 0 + @data, 0 + split /$split/, $data[0] );
 	}
 	
 	if( $step_proc_on ){
@@ -106,23 +108,17 @@ for (@ARGV){
 
 	@data = map { [ split /$split/ ] } @data;
 	
-	if( $to_pgm ){
-		print "P2\n";
-		print $cols, ' ', $rows, "\n";
-		print 255, "\n";
-	}
-	
-	my $grey = 2;
-	my $grey_on_dot = 3;
-	my $grey_on_middle = 4;
-	my $dot_on_middle = 5;
+	my $grey_on_empty = 'g';
+	my $grey_on_reccurence = 'G';
+	my $median_on_empty = 'm';
+	my $median_on_reccurence = 'M';
 
 	$debug and print "@{$_}\n" for @data;
 	$debug and print "---\n";
 
 	@data = reverse @data;
 	
-	my( $x, $y ) = (0, 0);
+	my( $x, $y ) = ( 0, 0 );
 	my @coords;
 	
 	while( $x < $rows and $y < $cols ){
@@ -134,14 +130,14 @@ for (@ARGV){
 			$row > $rows - 1 and next;
 			for my $col ($y .. $y - 1 + $col_step){
 				$col > $cols - 1 and next;
-				if( $data[ $row ][ $col ] =~ /1|$grey_on_dot/ ){
+				if( $data[ $row ][ $col ] =~ /1|$grey_on_reccurence/ ){
 					push @rows, $row;
 					push @cols, $col;
 					}
-				$data[ $row ][ $col ] == 0 
-					and $data[ $row ][ $col ] = $grey;
-				$data[ $row ][ $col ] == 1 
-					and $data[ $row ][ $col ] = $grey_on_dot;
+				$data[ $row ][ $col ] eq 0
+					and $data[ $row ][ $col ] = $grey_on_empty;
+				$data[ $row ][ $col ] eq 1 
+					and $data[ $row ][ $col ] = $grey_on_reccurence;
 				}
 			}
 		
@@ -161,8 +157,8 @@ for (@ARGV){
 		my $median_col = @cols % 2 ? $cols[ @cols / 2 ] 
 			: ( $cols[ @cols / 2 - 1 ] + $cols[ @cols / 2 ] ) >> 1;
 		
-		$data[ $median_row ][ $median_col ] =~ s/$grey/$grey_on_middle/ ||
-		$data[ $median_row ][ $median_col ] =~ s/$grey_on_dot/$dot_on_middle/;
+		$data[ $median_row ][ $median_col ] =~ s/$grey_on_empty/$median_on_empty/ ||
+		$data[ $median_row ][ $median_col ] =~ s/$grey_on_reccurence/$median_on_reccurence/;
 		
 		$debug and print "(rows:@rows|cols:@cols)"
 			. "[median_row:$median_row][median_col:$median_col]\n";
@@ -176,21 +172,52 @@ for (@ARGV){
 
 	if( $to_pgm ){
 		my %convert = (
-			0, 255,
-			1, 120,
-			$grey, 200,
-			$grey_on_dot, 80,
-			$grey_on_middle, 160,
-			$dot_on_middle, 0,
+			0 => 255,
+			1 => 180,
+			$grey_on_empty => 210,
+			$grey_on_reccurence => 150,
+			$median_on_empty => 230,
+			$median_on_reccurence => 0,
 		);
-		for (@data){
+		for( @data ){
 			map { $_ = $convert{ $_ } } @{$_};
 		}
 	}
-
+	
+	if( $to_ppm ){
+		my %convert = (
+			0 => '255 255 255',
+			1 => '180 180 180',
+			$grey_on_empty => '150 210 210',
+			$grey_on_reccurence => '100 150 150',
+			$median_on_empty => '0 255 255',
+			$median_on_reccurence => '0 0 0',
+		);
+		for( @data ){
+			map { $_ = $convert{ $_ } } @{$_};
+		}
+	}
+	
+	if( $to_pbm ){  # not relevant
+		print "P1\n";
+		print $cols, ' ', $rows, "\n";
+	}
+	
+	if( $to_pgm ){
+		print "P2\n";
+		print $cols, ' ', $rows, "\n";
+		print 255, "\n";
+	}
+	
+	if( $to_ppm ){
+		print "P3\n";
+		print $cols, ' ', $rows, "\n";
+		print 255, "\n";
+	}
+	
 	print do { local $" = $join; "@{$_}\n" } for @data;
 
-	if( !$to_pgm ){
+	if( !$to_pgm and !$to_ppm ){
 		print map "[$_]", join ',', map "($_)", 
 			map { join ',', map $_ + 1, @{$_} } @coords;
 		print "\n";
