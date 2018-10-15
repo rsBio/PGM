1c7b1174a76d759ec37b1278e1ca7abc5df14fdc
rsBio <dunedimensia@gmail.com>
2018-10-15 13:42:06 +0300

    minor, trees.pl

--- Trees/trees.pl
+++ Trees/trees.pl
@@ -7,41 +7,48 @@ srand;
 sub findxy {
 	my( $Ax, $Ay, $Bx, $By, $ab, $ac, $bc ) = @_;
 	my( $x, $y ) = (0, 0);
+	my( $midX, $midY ) = ( ($Ax + $Bx) / 2, ($Ay + $By) / 2 );
 	for my $i (1 .. 80){
-	my $step = 1;
-	while( $step > 0.000000001 ){
-	#	use bignum;
-	#	my $sort = int rand(2);
-		my ($ref_closer) = 
-		# Schwartzian transform:
-		map { [ $_->[0], $_->[1] ] }
-		(
-		sort { $a->[2] <=> $b->[2] }
-		map {
-			my( $x1, $y1 ) = @{ $_ };
-			[ $x1, $y1,
-			abs( $ac - sqrt( ($Ax - $x1)**2 + ($Ay - $y1)**2 ) ) +
-			abs( $bc - sqrt( ($Bx - $x1)**2 + ($By - $y1)**2 ) )
-			]
+		my $step = 1;
+		while( $step > 0.000000001 ){
+		#	use bignum;
+		#	my $sort = int rand(2);
+			my ($ref_closer) = 
+			# Schwartzian transform:
+			map { [ $_->[0], $_->[1] ] }
+			(
+			sort { $a->[2] <=> $b->[2] }
+			map {
+				my( $x1, $y1 ) = @{ $_ };
+				[ $x1, $y1,
+				abs( $ac - sqrt( ($Ax - $x1)**2 + ($Ay - $y1)**2 ) ) +
+				abs( $bc - sqrt( ($Bx - $x1)**2 + ($By - $y1)**2 ) )
+				]
+			}
+				[ $x, $y ],
+				[ $x + $step, $y + $step ],
+				[ $x + $step, $y - $step ],
+				[ $x - $step, $y + $step ],
+				[ $x - $step, $y - $step ],
+				[ $x + $step, $y + 0 ],
+				[ $x + $step, $y - 0 ],
+				[ $x - 0, $y + $step ],
+				[ $x - 0, $y - $step ],
+			)[ 0 ];
+		#	no bignum;	
+			($x, $y) = @{$ref_closer};
+		#	print "[$x|$y]\n";
+		#	$step *= 0.7 + (0.2 * rand(1));
+			$step *= 0.5;
 		}
-			[ $x, $y ],
-			[ $x + $step, $y + $step ],
-			[ $x + $step, $y - $step ],
-			[ $x - $step, $y + $step ],
-			[ $x - $step, $y - $step ],
-			[ $x + $step, $y + 0 ],
-			[ $x + $step, $y - 0 ],
-			[ $x - 0, $y + $step ],
-			[ $x - 0, $y - $step ],
-		)[ 0 ];
-	#	no bignum;	
-		($x, $y) = @{$ref_closer};
-	#	print "[$x|$y]\n";
-	#	$step *= 0.7 + (0.2 * rand(1));
-		$step *= 0.5;
 	}
-	}
-	"[$x|$y]\n"
+	my( $symX, $symY );
+	if( $x < $midX ){ $symX = $midX + ($midX - $x) }
+	else { $symX = $midX - ($x - $midX) }
+	if( $y < $midY ){ $symY = $midY + ($midY - $y) }
+	else { $symY = $midY - ($y - $midY) }
+	
+	"[$x|$y||$symX|$symY]\n"	
 }
 
 #print findxy( $Ax, $Ay, $Bx, $By, $ab, $ac, $bc );
