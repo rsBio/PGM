e39d47ed722b69a5d5c612415eb16b1397e6f6d9
lynx <dunedimensia@gmail.com>
2021-09-01 15:51:37 +0300

    minor

--- Matricos/gen_sin.pl
+++ Matricos/gen_sin.pl
@@ -16,13 +16,16 @@ srand;
 
 my $dim = 1;
 my $rand = 0;
-my $wide = 100;
+my $length = 100;
 my $join = " ";
 my $deletions = 0;
 
 for( @opt ){
 	/-dim(\d+)/ and do {
 		$dim = $1;
+	};
+	/-length(\d+)/ and do {
+		$length = $1;
 	};
 	/-rand(\d+)/ and do {
 		$rand = $1;
@@ -48,17 +51,17 @@ for( @opt ){
 @FILES and die "$0: Should not contain files.\n";
 
 for my $dim ( 1 .. $dim ){
-	my @arr = map { 1 + sin $_ } map { $_ / 3 } 1 .. $wide;
+	my @arr = map { 1 + sin $_ } map { $_ / 3 } 1 .. $length;
 	$debug and print "arr(sin):@arr\n";
 	
 	if( $deletions ){
 		OUTER:
-		for my $w ( map { ( $_ ) x 3 } 1 .. $wide / 4 ){
+		for my $w ( map { ( $_ ) x 3 } 1 .. $length / 4 ){
 			
 			for my $deletion ( 1 .. rand( 1 + 1 / $w ** 1 ) ){
 				$debug and print $w . "\n";
 				
-				my $start = int rand( $wide - $w - 1 );
+				my $start = int rand( $length - $w - 1 );
 				@arr[ $start .. $start + $w - 1 ] = ( 0 ) x $w;
 				
 				redo OUTER;
