7722603e782803cf74c0e3aa6247088437a4e993
lynx <dunedimensia@gmail.com>
2021-09-01 14:41:24 +0300

    minor

--- Matricos/gen_sin.pl
+++ Matricos/gen_sin.pl
@@ -5,11 +5,11 @@ use strict;
 
 my $debug = 0;
 
-my @ARGV_2;
+my @FILES;
 my @opt;
 
-for (@ARGV){
-	/^-\S/ ? (push @opt, $_) : (push @ARGV_2, $_);
+for( @ARGV ){
+	/^-\S/ ? ( push @opt, $_ ) : ( push @FILES, $_ );
 }
 
 srand;
@@ -20,7 +20,7 @@ my $wide = 100;
 my $join = " ";
 my $deletions = 0;
 
-for (@opt){
+for( @opt ){
 	/-dim(\d+)/ and do {
 		$dim = $1;
 	};
@@ -42,35 +42,30 @@ for (@opt){
 	/-tossv/ and do {
 		$join = ' ';
 	};
-	/-d\b/ and $debug = 1;
+	/-d$/ and $debug = 1;
 }
 
-@ARGV = @ARGV_2;
+@FILES and die "$0: Should not contain files.\n";
 
-@ARGV and die "$0: Should not contain args.\n";
-
-for my $dim (1 .. $dim){
+for my $dim ( 1 .. $dim ){
 	my @arr = map { 1 + sin $_ } map { $_ / 3 } 1 .. $wide;
 	$debug and print "arr(sin):@arr\n";
-#	map { $_ = rand(2) } @arr[ 10 + rand($rand) - $rand / 2 .. 20 + rand($rand) - $rand / 2];
-#	$debug and print "arr(rand):@arr\n";
 	
-	print "@arr\n";
-	
-	OUTER:
-	for my $w ( map { ( $_ ) x 3 } 1 .. $wide / 4 ){
-		
-		for my $deletion ( 1 .. rand( 1 + 1 / $w ** 1 ) ){
-			$debug and print $w . "\n";
+	if( $deletions ){
+		OUTER:
+		for my $w ( map { ( $_ ) x 3 } 1 .. $wide / 4 ){
 			
-			my $start = int rand( $wide - $w - 1 );
-			@arr[ $start .. $start + $w - 1 ] = ( 0 ) x $w;
-			
-			redo OUTER;
+			for my $deletion ( 1 .. rand( 1 + 1 / $w ** 1 ) ){
+				$debug and print $w . "\n";
+				
+				my $start = int rand( $wide - $w - 1 );
+				@arr[ $start .. $start + $w - 1 ] = ( 0 ) x $w;
+				
+				redo OUTER;
+				}
 			}
 		}
-		
-#	print "@arr\n";
+	
 	print map "$_\n", join ' ', grep $_, @arr;
 }
 
